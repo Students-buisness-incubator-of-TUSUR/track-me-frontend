b90d4317b3c7982b1b2265d0588be9359ee1252d
BDA228 <131526143+BDA228@users.noreply.github.com>
2026-06-17 15:12:33 +0700

    Исправил тесты

--- src/accets/komand/team-card.test.js
+++ src/accets/komand/team-card.test.js
@@ -1424,7 +1424,7 @@ describe("checkMeetingCreation max meetings", () => {
     await waitForLoad();
     fireEvent.click(screen.getByRole("button", { name: /запланировать/i }));
     await waitFor(() =>
-      expect(screen.getByTestId("meeting-error")).toHaveTextContent(/максимальное количество/i)
+      expect(screen.getByTestId("meeting-error")).toHaveTextContent(/максимально возможное число встреч/i)
     );
   });
 });
